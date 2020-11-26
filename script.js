32ff3515b5f8590393310e9f3d6405204cbcf9ff
criativo303 <73407008+MariaTeixeira23@users.noreply.github.com>
2020-11-26 11:42:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.body.innerHTML
+
+/* MENU DO ABOUT*/
+
+
+  /* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "450px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
+
+
+  /* MENU DO WORK*/
+
+
+  function openNavwork() {
+    document.getElementById("mySidenavwork").style.width = "450px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNavwork() {
+    document.getElementById("mySidenavwork").style.width = "0";
+  }
+  
+/*
+  function openNavwork() {
+    document.getElementById("mySidenavwork").style.width = "450px";
+    document.getElementById("main2").style.marginLeft = "450px";
+  }
+  
+  function closeNavwork() {
+    document.getElementById("mySidenavwork").style.width = "0";
+    document.getElementById("main2").style.marginLeft = "0";
+  } */
